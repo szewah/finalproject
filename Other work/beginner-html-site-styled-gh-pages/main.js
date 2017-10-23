1ebc02c40db4acf18378562f3cea905db397838a
szewah <32065713+szewah@users.noreply.github.com>
2017-10-23 13:55:02 -0400

    RGB color choice

--- Other work/beginner-html-site-styled-gh-pages/main.js
+++ Other work/beginner-html-site-styled-gh-pages/main.js
@@ -1,25 +1,35 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent='Hello World!';
 
-var myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-	var mySrc=myImage.getAttribute('scr');
-	if (mySrc==='images/firefox-icon.png') {
-	myImage.setAttribute('scr','images/shopimage.jpg');
-	} else {
-		myImage.setAttribute('scr','images/firefox-icon.png');
-	}	
-}
-
 
 var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/firefox2.png');
+      myImage.setAttribute ('src','images/shop-image.png');
     } else {
       myImage.setAttribute ('src','images/firefox-icon.png');
     }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+
+myButton.onclick = function() {
+  setUserName();
 }
---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,26 @@
+var num1 = 10 * 10;
+document.write(num1);
+
+var num2 = 10 / 10;
+document.write(num2);
+
+var num3 = 50-17;
+document.write(num3);
+
+var num4 = 50+17;
+document.write(num4);
+
+var myName = ' Szewah Chin '
+document.write(myName);
+
+
+function hello () {
+	  var greeting = 'hello!';
+	  greeting = 'hello, world!';
+	  // alert(greeting);
+	}
+
+// hello()
+
+document.querySelector('#title').style.color = 'red';
+document.querySelector('#text-chunk').style.color = 'green';
