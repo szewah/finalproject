e8d1c53f870c04c7d0bb2f8201ab8b5b8d7f6631
szewah <32065713+szewah@users.noreply.github.com>
2017-11-02 09:19:32 -0400

    list-maker

--- Homework/list-maker/js/app.js
+++ Homework/list-maker/js/app.js
@@ -4,7 +4,8 @@
 // 3. New item is appended as a <li> element to the #list
 // 4. After item is added, the text inside #item should clear
 
-// Requirements: Create a separate function, called appendItem, that accepts one argument, item, that is called when #clickme is clicked and appends the <li> to #list
+// Requirements: Create a separate function, called appendItem, that accepts one argument, item, 
+// that is called when #clickme is clicked and appends the <li> to #list
 
 // Bonus: Focus on #item after the item is added (research jQuery focus method)
 // Itermediate Bonus: If the value of #item is blank, do not append the <li> and alert user (if/else conditions)
@@ -17,3 +18,41 @@
 // 4. handleClick should call a separate function, named appendItem, and pass it the variable newItem
 // 5. handleClick should then clear the text inside of #item
 // 6. Finally, write the function, appendItem (see above)
+
+$('#clickme').click(handleClick); 
+
+function handleClick() {
+	var newItem = $('#item').val();
+	appendItem(newItem);
+	if(newItem) {
+		$('#item').val();
+	} else {
+		alert('Please add an item');
+	}
+	$('#item').val('');
+}
+
+
+function appendItem(text){
+	// $('#list').html(text);
+	$('#list').append('<li>' + text + '</li>');
+}
+
+// function clearItem(clear) {
+// 	$('item').val()
+// }
+
+
+// $('#submit').click(function () {
+//   // 1. create a variable to store the value from #input-name
+//    var input = $('#input-name').val(); 
+//    // 2. call the function writeName; make sure to pass in the variable you just created as an argument!
+//   writeName(input);
+// })
+
+// function writeName (name) {
+//   // 3. output `name` into #output-name using .text() or .html()
+//   $('#output-name').html(name);
+// }
+
+
